c895fb6cad771c1ecad4157456bee48c98dac040
Martin Matysiak <kaktus621@gmail.com>
2011-10-30 01:52:33 +0200

    adjusted the code style according to PEP 8

--- rawread.py
+++ rawread.py
@@ -11,29 +11,28 @@ by Martin Matysiak
 
 __version__ = "2.2a1"
 
-import sys
 import os
+import sys
 from optparse import OptionParser
 
-# some pseudoconstant values
-# the part of the header which identifies a nofs
+# Some pseudoconstant values
+# The part of the header which identifies a nofs
 NOFS_SIGNATURE = "k621.de"
 
-# the full filesystem header
+# The full filesystem header
 NOFS_HEADER = NOFS_SIGNATURE + "\0\0\0\0"
 
-# the terminal symbol which identifies the end of a nofs
+# The terminal symbol which identifies the end of a nofs
 NOFS_TERMINAL = chr(0x03)
 
-# the sector size which is used for the nofs
+# The sector size which is used for the nofs
 NOFS_SECTOR_SIZE = 512
 
-# the maximum amount of devices that should be scanned (starting from ..0/..a)
+# The maximum amount of devices that should be scanned (starting from ..0/..a)
 MAX_DEVICES = 16
 
-# a more object-oriented approach to the problem
 class Device:
-    """A class representing an input device"""
+    """A class representing an input device."""
     
     valid_nofs = False
     sectors_read = None
@@ -41,7 +40,7 @@ class Device:
     _force_operations = False
     _device_handle = None
     
-    def __init__(self, path, force = False, permissions = "r"):
+    def __init__(self, path, force=False, permissions="r"):
         """Class constructor. 
         
         Parameters:
@@ -53,18 +52,19 @@ class Device:
         
         May throw IOError if the device doesn't exist or can't be accessed
         """
-        # check if the device exists at all
+        # Check if the device exists at all
         if not os.path.exists(path):
             raise IOError("Given device doesn't exist")
         
-        # try to access the device with the given permissions
+        # Try to access the device with the given permissions
         self._device_handle = file(path, permissions)
         
-        # read the first few bytes to assure there is a nofs on it
-        self.valid_nofs = self._device_handle.read(len(NOFS_SIGNATURE)) == NOFS_SIGNATURE
+        # Read the first few bytes to assure there is a nofs on it
+        self.valid_nofs = (self._device_handle.read(len(NOFS_SIGNATURE)) == 
+                           NOFS_SIGNATURE)
         self._device_handle.seek(0)
         
-        # remember parameters
+        # Remember parameters
         self._force_operations = force   
     
     def __del__(self):
@@ -87,9 +87,9 @@ class Device:
         self._device_handle.seek(0)
         self.sectors_read = 0
 
-        # If we have a valid NoFS, ignore the first few bytes so that
-        # they don't show up in the returned content
-        # Furthermore, set up which symbols will end the content getting
+        # If we have a valid NoFS, ignore the first few bytes so that they
+        # don't show up in the returned content.  Furthermore, set up which 
+        # symbols will end the content getting.
         terminal = chr(0x00)
         if self.valid_nofs:
             self._device_handle.read(len(NOFS_HEADER))
@@ -115,8 +115,7 @@ class Device:
         
         return result
 
-        
-    def erase(self, complete = False):
+    def erase(self, complete=False):
         """Erases the device or file.
         
         Parameters:
@@ -126,14 +125,14 @@ class Device:
         Returns True if erase procedure succeeded, otherwise False (e.g. if 
         no valid NoFS was detected and force set to False). No writing 
         writing operations have been performed if this method returns False!
-        
+
         """
         if not (self.valid_nofs or self._force_operations):
             return False
 
-        # If only a partial erase is wished and the count of sectors
-        # hasn't been determined, sweep through the contents
-        if self.sectors_read == None and not complete:
+        # If only a partial erase is wished and the count of sectors hasn't 
+        # been determined, sweep through the contents.
+        if self.sectors_read is None and not complete:
             self.get_contents()
             
         result = self._erase_sectors(self.sectors_read if not complete else -1)
@@ -153,14 +152,17 @@ class Device:
         
         """
         if self.valid_nofs:
-            print "ERROR: the device already contains a valid NoFS. Initialization aborted"
+            print "ERROR: the device already contains a valid NoFS. \
+                   Initialization aborted"
             return False
 
         if not self._force_operations:
             return False
         
-        # erase the first few sectors and write header
-        self._erase_sectors(4,1)
+        # Erase the first few sectors and write header. Please note: while
+        # erasing, a NOFS_TERMINAL will be placed inside every first byte of
+        # an erased sector.
+        self._erase_sectors(4, 1)
         self._write_header()
         return True
     
@@ -169,17 +171,17 @@ class Device:
         first sector"""
         self._device_handle.seek(0)
 
-        # remember: we can only write a whole sector at a time
+        # Remember: we can only write a whole sector at a time
         first_sector = NOFS_HEADER
         first_sector += NOFS_TERMINAL
-        first_sector += (NOFS_SECTOR_SIZE - len(NOFS_HEADER) - len(NOFS_TERMINAL)) * chr(0xFF)
+        first_sector += (NOFS_SECTOR_SIZE - len(NOFS_HEADER) - 
+                         len(NOFS_TERMINAL)) * chr(0xFF)
         
-        # write it onto the device
+        # Write it onto the device
         self._device_handle.write(first_sector)
         self._device_handle.flush()
 
-    
-    def _erase_sectors(self, count = 0, start = 0):
+    def _erase_sectors(self, count=0, start=0):
         """Internal method for erasing a specific count of sectors on
         the device. A sector is called erased when the very first byte
         is a NOFS_TERMINAL and the rest 0xFF. The NOFS_TERMINAL serves
@@ -207,11 +209,12 @@ class Device:
                 eof = True
         
         # If we got an exception before count was less than or equal zero,
-        # something went wrong
+        # something went wrong.
         return count <= 0
 
-# returns a list of possible device names, depending on the running os
+
 def get_possible_devices():
+    """Returns a list of possible device names, depending on the running OS"""
     possible_devices = []
     if sys.platform.startswith("win32"):
         for i in range(MAX_DEVICES):
@@ -225,11 +228,15 @@ def get_possible_devices():
 
     return possible_devices
 
+
 def get_removable_devices(opts):
-    # detect the correct device by letting the user eject and insert the device
+    """Method which is used to determine devices that might be
+    removable drives.
+    """
+    # Detect the correct device by letting the user eject and insert the device
     raw_input("> Please eject the device if inserted and press enter...")
     
-    # scan through a list of possible devices and remember the available ones
+    # Scan through a list of possible devices and remember the available ones
     available_devices = []
     
     for device in get_possible_devices():
@@ -241,7 +248,7 @@ def get_removable_devices(opts):
         except IOError:
             pass
     
-    if len(available_devices) == 0:
+    if not available_devices:
         print "\nERROR: Available devices could not be determined. Root\
               \npermissions may be necessary to perform this task. No action\
               \nwill be performed.\n"
@@ -250,15 +257,15 @@ def get_removable_devices(opts):
     
     raw_input("> Please insert the device now and press enter...")
     
-    # scan again, remeber the differences
+    # Scan again, remeber the differences
     additional_devices = []
     
-    # create a list of devices with possible candidates
+    # Create a list of devices with possible candidates
     left_devices = get_possible_devices()
     for device in available_devices:
         left_devices.remove(device)
     
-    # scan those devices again and check if something has changed    
+    # Scan those devices again and check if something has changed    
     for device in left_devices:
         try:
             handle = open(device, "rb")
@@ -269,22 +276,26 @@ def get_removable_devices(opts):
             pass
     
     return additional_devices
+ 
     
 def get_nofs_device():
-    # scan through possible devices and check for nofs header
+    """Returns the first device found which contains a valid NoFS"""
+    
     for device in get_possible_devices():
         try:
             dev = Device(device)
         except IOError:
             pass
     
-    # if the program reaches this point, no device could be found
+    # If the program reaches this point, no device could be found
     print "ERROR: No NoFS device could be found"
     sys.exit(3)
 
-# main program
+
 def main():
-    # check arguments
+    """Main entry point of the application"""
+
+    # Check passed command line arguments
     parser = OptionParser(version="\nRawRead %s\n  by Martin Matysiak\
                           \n  www.martin-matysiak.de\n" % __version__)
     parser.add_option("-i", "--input", dest="input_device", help="read data "
@@ -299,13 +310,13 @@ def main():
                       "if device contains valid NoFS or -f is set. If used in "
                       "combination with -o, the device will be read out first "
                       "and erased afterwards")
-    parser.add_option("-E", "--full-erase", action="store_true", dest="full_erase",
-                      default=False, help="disk will be completely overwritten. "
-                      "May take a long time depending on the size of the device "
-                      "specified by INPUT. will only be performed if device "
-                      "contains valid NoFS or -f is set. If used in combination "
-                      "with -o, the device will be read out first and erased "
-                      "afterwards.")
+    parser.add_option("-E", "--full-erase", action="store_true", 
+                      dest="full_erase", default=False, help="disk will be "
+                      "completely overwritten. May take a long time depending "
+                      "on the size of the device specified by INPUT. will "
+                      "only be performed if device contains valid NoFS or "
+                      "-f is set. If used in combination with -o, the device "
+                      "will be read out first and erased afterwards.")
     parser.add_option("-f", "--force", action="store_true", dest="force",
                       default=False, help="force actions, no matter if the "
                       "input device contains a NoFS or not. DANGEROUS!")
@@ -316,22 +327,24 @@ def main():
 
     (options, args) = parser.parse_args()
     
-    # handle trivial parameter errors
+    # Handle trivial parameter errors
     if options.erase_disk and options.full_erase:
         parser.error("can't use -e and -E at the same time!")
         
     if options.create_disk and options.output_file:
-        parser.error("can't use -c and specify an output file at the same time!")
+        parser.error("can't use -c and specify an output file at "
+                     "the same time!")
         
     if options.create_disk and (options.erase_disk or options.full_erase):
         parser.error("can't use -c and -e or -E at the same time!")
         
     if options.create_disk and options.input_device and not options.force:
-        parser.error("can't use -c and -i unless -f is set (are you SURE you want to do this?)")
+        parser.error("can't use -c and -i unless -f is set (are you "
+                     "SURE you want to do this?)")
 
-    # if -c is set, perform a special creation routine:
+    # If -c is set, perform a special creation routine:
     if options.create_disk:
-        # check if device is given or if it shall be determined automatically
+        # Check if device is given or if it shall be determined automatically
         devices = []
         if options.input_device:
             if options.force:
@@ -341,16 +354,16 @@ def main():
         else:
             devices = get_removable_devices(options)
         
-        if len(devices) == 0:
+        if not devices:
             print "\nERROR: No suitable device could be found.\n"
         elif len(devices) >= 2:
-            print "\nWARNING: More than one suitable device has been detected. No\
-                  \naction will be performed. Please run the program again, using\
-                  \nthe -i parameter to select one of the following devices and -f\
-                  \nto enforce your selection (dangerous!):\n"
+            print "\nWARNING: More than one suitable device has been detected.\
+                  \nNo action will be performed. Please run the program again,\
+                  \nusing the -i parameter to select one of the following\
+                  \ndevices and -f to enforce your selection (dangerous!):\n"
             print devices
         else:
-            # create a nofs on the device
+            # Create a nofs on the device
             print "> Initializing NoFS on %s..." % devices[0]
             if (Device(devices[0], True, "rb+").initialize_nofs()):
                 print "> Device initialized successfully!"    
@@ -359,9 +372,8 @@ def main():
         
         sys.exit(0)
 
-    # otherwise proceed as following:
-
-    # select input device
+    # Otherwise proceed as following:
+    # 1) Select the input device
     input_handle = None
     device = None
     permissions = "rb+" if options.erase_disk or options.full_erase else "rb"
@@ -370,26 +382,26 @@ def main():
     if options.input_device:
         device = Device(options.input_device, options.force, permissions)
     else:
-        # search for a nofs device
+        # Search for a nofs device
         for device_path in get_possible_devices():
             try:
                 device = Device(device_path, options.force, permissions)
             except IOError as ex:
-                # ignore this device and turn to the next one
+                # Ignore this device and turn to the next one
                 print "WARNING (%s): %s" % (device_path, str(ex))
                 continue
 
             if device.valid_nofs:
                 break
         
-        if device == None or not device.valid_nofs:        
+        if device is None or not device.valid_nofs:        
             print "ERROR: No valid NoFS device found"
             sys.exit(0)
 
-    # read input device
+    # 2) Read from the input device
     device_contents = device.get_contents()
 
-    # open output stream
+    # 3) Open an output stream
     output_handle = None
 
     if options.output_file:
@@ -397,21 +409,22 @@ def main():
     else:
         output_handle = sys.stdout
 
-    # check if it should be erased
+    # 4) Check if it should be erased
     if options.erase_disk or options.full_erase:
         if not device.erase(options.full_erase):
             print "ERROR: Couldn't erase device"
-        # if device-erase has been set, write only if output is not stdout
+        # If device-erase has been set, write only if output is not stdout
         if options.output_file:
             output_handle.write(device_contents)
     else:
         output_handle.write(device_contents)
 
-    # check if output file was given, otherwise stdout would be closed
+    # 5) Close the output stream (if it's not stdout)
     if options.output_file:
         output_handle.close()
 
     del device
+
 
 if __name__=='__main__':
   main()
